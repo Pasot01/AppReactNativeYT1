39feb7bfc2cfad2470624bd29612d798dc118f21
Pasot01 <contactmyphone14@gmail.com>
2023-08-20 23:35:19 +0200

    configuration du header et mise en place d'une flatlist horizontal

--- ecrans/home/index.js
+++ ecrans/home/index.js
@@ -1,12 +1,40 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image, FlatList } from 'react-native'
 import React from 'react'
+import dashboardStyles from './style';
+import { FakeActivity } from './../../fakeData/fakeActivity';
 
 const Home = () => {
     return (
-        <View>
-            <Text>Ecran Accueil</Text>
-        </View>
-    )
-}
+        <ScrollView>
+
+            {/* Début du header  */}
+            <View style={dashboardStyles.header}>
+                <Text style={dashboardStyles.userName}>John Doe</Text>
+                <Image
+                    source={require('./../../assets/woman.jpg')}
+                    style={dashboardStyles.userImg} 
+                />
+            </View>
+            {/* Fin du header */}
+
+            {/* Liste des activités */}
+            <FlatList
+                data={FakeActivity}
+                keyExtractor={item => item.id}
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                renderItem={({ item }) => {
+                    return (
+                        <View>
+                            <Text>{item.mainText}</Text>
+                        </View>
+                    );
+                }}
+            />
+
+            {/* Fin de liste des activités */}
+        </ScrollView>
+    );
+};
 
 export default Home
---
+++ ecrans/home/style.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+import { PADDING } from "../../outils/constantes";
+
+const dashboardStyles = StyleSheet.create({
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: PADDING.horizontal,
+        paddingVertical: PADDING.vertical,
+        backgroundColor: 'white',
+    },
+    userImg: {
+        width: 50,
+        height: 50,
+        borderRadius: 50 / 2,
+    },
+    userName: {
+        fontSize: 16,
+    },
+});
+
+export default dashboardStyles
